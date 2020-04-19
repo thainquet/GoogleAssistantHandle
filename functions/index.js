c8118a0175857c7ed7b09907077723b75a4459d4
thainquet <thainq00@gmail.com>
2020-04-19 23:43:46 +0700

    add other handle function

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-const {dialogflow, SignIn} = require('actions-on-google');
+const { dialogflow, SignIn } = require('actions-on-google');
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {Suggestion} = require('dialogflow-fulfillment');
+const { Suggestion, BasicCard, Image } = require('dialogflow-fulfillment');
 
-
-const agent = new WebhookClient({request: request, response: response});
 const app = dialogflow({
   clientId: '596655066542-3rl4j8of2gt610roab8dg4vo1b8h189r.apps.googleusercontent.com',
   debug: true
@@ -15,19 +12,35 @@ process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 app.intent('Default Welcome Intent', (conv) => {
   // Do things
   conv.ask('Welcome, maxflow user');
-  conv.ask("Not signed in yet.");
-  conv.ask(new Suggestion("want to sign in"));
 });
 
-app.intent('Sign in', (conv, params, signin) => {
-  conv.ask(new SignIn());
-  if (signin.status !== 'OK') {
-    return conv.ask('You need to sign in before using the app.');
-  }
-  const access = conv.user.access.token;
-  console.log("access token", access);
-  // possibly do something with access token
-  return conv.ask('Great! Thanks for signing in.');
+// Intent that starts the account linking flow.
+app.intent('Start Sign-in', (conv) => {
+  conv.ask(new SignIn('To get your account details'));
 });
 
-exports.dialogflowFirebaseFulfillment  = functions.https.onRequest(app);
+app.intent("Show Me", conv => {
+  const payload = conv.user.profile.payload;
+  let email = payload.email;
+  conv.ask(`your email: ${email}`);
+});
+
+app.intent("Run Workflow", conv => {
+  const payload = conv.user.profile.payload;
+  let email = payload.email;
+  // do sth
+});
+
+app.intent("Get Workflow List", conv => {
+  const payload = conv.user.profile.payload;
+  let email = payload.email;
+  conv.ask("note, thai 1");
+});
+
+app.intent("Get Workflow Status", conv => {
+  const payload = conv.user.profile.payload;
+  let email = payload.email;
+  conv.ask("success");
+});
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
